9c962e9d0af2e8fb6df20ff1faad3809af3c325a
Elmer Rocha <elmer.rocha@correo.uis.edu.co>
2020-03-31 18:35:20 -0500

    Arreglos

--- app/src/main/java/com/dye/heartratebyecg/Grafica.java
+++ app/src/main/java/com/dye/heartratebyecg/Grafica.java
@@ -59,7 +59,7 @@ public class Grafica extends IOIOActivity {
     OutputStream outputStream;
     private int ConteoPDF = 1;
     private Bitmap Imagen;
-
+    //Probando las subidas
     /*-----------------------------------------*/
 
     /*-------- Variables temporizador ---------*/
